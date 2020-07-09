bf62c9d6a6084eaf91305063f08cf9cb6c03b226
hassanmujtaba <hassan03203406130@gmail.com>
2020-07-09 21:07:29 +0500

    build created

---
+++ build/precache-manifest.b0f49bb259e44026b8361d6df3a3fe18.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8968e17aec12a0f6cc85a2063112963f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "ad151f8f02b445f62ffd",
+    "url": "/static/css/main.60cceaa8.chunk.css"
+  },
+  {
+    "revision": "c04e83d6217c56135b1b",
+    "url": "/static/js/2.7edca1fd.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.7edca1fd.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ad151f8f02b445f62ffd",
+    "url": "/static/js/main.410f9175.chunk.js"
+  },
+  {
+    "revision": "dc270c05f046063e7700",
+    "url": "/static/js/runtime-main.b96d0c24.js"
+  }
+]);
